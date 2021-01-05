b941aaf0bf41fec89f1aab0f63111e425ad86c8e
shivani-2020 <bhattshivani64@gmail.com>
2021-01-05 18:09:20 -0500

    first commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: "mongodb+srv://root:root@mydb.qr7oy.mongodb.net/LoginData?retryWrites=true&w=majority" 
+  };
+
+  module.exports = {
+    mongoURI: "mongodb+srv://root:root@mydb.qr7oy.mongodb.net/LoginData?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
